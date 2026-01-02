1e22d7c396a7b5c8cabbd7be235216acf709566d
Luis Starlino <luis.guilherme009@gmail.com>
2026-01-02 20:04:39 -0300

    feat(core): finish the domain core conifgurations

---
+++ StarLedger.Domain/Entities/LedgerAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarLedger.Domain.Entities
+{
+    public class LedgerAccount
+    {
+        private readonly List<LedgerEntry> _entries = new();
+        public decimal Balance => _entries.Sum(e => e.Type == Enums.EntryType.Credit ? e.Amount : -e.Amount);
+        public void AddEntry(LedgerEntry entry)
+        {
+            if (entry.Type == Enums.EntryType.Debit && Balance < entry.Amount)
+                throw new InvalidOperationException("You dont have credits!");
+
+            _entries.Add(entry);
+        }
+    }
+}
