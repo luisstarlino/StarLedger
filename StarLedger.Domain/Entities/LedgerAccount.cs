fd5c1662665bd185a2e27cf1e53a5ec4063bc9ac
Luis Starlino <luis.guilherme009@gmail.com>
2026-01-07 21:31:31 -0300

    feat(complete):finish all the project

--- StarLedger.Domain/Entities/LedgerAccount.cs
+++ StarLedger.Domain/Entities/LedgerAccount.cs
@@ -10,7 +10,7 @@ namespace StarLedger.Domain.Entities
         public decimal Balance => _entries.Sum(e => e.Type == Enums.EntryType.Credit ? e.Amount : -e.Amount);
         public void AddEntry(LedgerEntry entry)
         {
-            if (entry.Type == Enums.EntryType.Debit && Balance < entry.Amount)
+            if (entry.Type == Enums.EntryType.Debit && Balance == 0 || Balance > entry.Amount)
                 throw new InvalidOperationException("You dont have credits!");
 
             _entries.Add(entry);
---
+++ StarLedger.Infrastructure/Pesistence/InMemoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using StarLedger.Application.Interfaces;
+using StarLedger.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StarLedger.Infrastructure.Pesistence
+{
+    public class InMemoryRepository : ILedgerRepository
+    {
+        // --- IN MEMORY STORE
+        private LedgerAccount _account = new();
+
+
+        public LedgerAccount Get()
+        {
+            return _account;
+        }
+
+        public void Save(LedgerAccount account)
+        {
+            _account = account;
+        }
+    }
+}
